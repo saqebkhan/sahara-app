33cda008b49ae3d7b1a8d18a6895ca48374db354
saqebkhan <saqebk619@gmail.com>
2024-06-03 23:52:48 +0530

    Fixed bug

--- src/components/modules/Inmates/AddEditForm.vue
+++ src/components/modules/Inmates/AddEditForm.vue
@@ -8,9 +8,7 @@
         </h2>
         <div class="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
           <div class="sm:col-span-3">
-            <label
-              for="full-name"
-              :class="commonClassesFuncs.commonLabelClasses()"
+            <label for="full-name" :class="commonClasses.labelClasses"
               >Full name</label
             >
             <div class="mt-2">
