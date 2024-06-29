a3c7296ac38daf71cf6c8b4587ed624c73e9c375
saqebkhan <saqebk619@gmail.com>
2024-06-29 17:04:40 +0530

    Added second ui slots

--- src/components/modules/Slots/Slots.vue
+++ src/components/modules/Slots/Slots.vue
@@ -31,83 +31,108 @@
         </button>
       </div>
     </div>
-    <div
-      class="py-8 max-sm:px-0 px-4 max-w-screen-xl text-center lg:py-16 lg:px-6"
-    >
-      <div
-        class="grid gap-8 max-sm:grid-cols-3 lg:gap-16 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6"
-      >
-        <div v-if="store.selectedHostel !== 'SAHARA GROUP OF HOSTELS'">
-          <h2 class="font-extrabold text-2xl max-sm:text-xl">
-            {{ store.selectedHostel }}
-          </h2>
-          <div
-            v-for="(room, roomIndex) in store.selectedHostel"
+    <table class="min-w-full divide-y divide-gray-300 overflow-y-auto mt-12">
+      <thead class="sticky top-0">
+        <tr class="bg-indigo-600 pr-4 divide-x">
+          <!-- Header columns -->
+          <th scope="col" :class="commonHeaderClasses">
+            <a href="#" :class="[commonAnchorClasses, 'text-nowrap']"
+              >SR. NO.</a
+            >
+          </th>
+          <th scope="col" :class="commonHeaderClasses">
+            <a href="#" :class="commonAnchorClasses">HOSTEL</a>
+          </th>
+          <th scope="col" :class="commonHeaderClasses">
+            <a href="#" :class="commonAnchorClasses">ROOM NUMBER</a>
+          </th>
+          <th scope="col" :class="commonHeaderClasses">
+            <a href="#" :class="commonAnchorClasses">BED</a>
+          </th>
+          <th scope="col" :class="commonHeaderClasses">
+            <a href="#" :class="commonAnchorClasses">SHARING BED</a>
+          </th>
+          <th scope="col" :class="commonHeaderClasses">
+            <a href="#" :class="commonAnchorClasses">VACANT FILTER</a>
+          </th>
+        </tr>
+      </thead>
+      <tbody class="divide-y divide-gray-200 bg-white">
+        <!-- Table rows -->
+        <tr
+          v-for="(slot, index) in slots"
+          :key="slot._id"
+          class="hover:bg-indigo-50 divide-x"
+        >
+          <td
+            class="whitespace-nowrap bg-indigo-50 py-5 text-sm font-medium sm:pl-0 flex"
           >
-            <div
-              v-for="(bed, bedIndex) in room.beds"
-              :key="`room_${roomIndex}_bed_${bedIndex}`"
-            >
-              <div class="text-center text-gray-500">
-                <div
-                  class="w-10 h-20 max-sm:w-10 max-sm:h-15 mx-auto bg-gray-300 rounded-lg"
-                ></div>
-                <h3
-                  class="mb-1 text-2xl max-sm:text-lg font-bold tracking-tight text-gray-900"
-                >
-                  <a href="#">{{
-                    bed.status === "occupied" ? "Occupied" : "Vacant"
-                  }}</a>
-                </h3>
-                <p>Bed - {{ bed.bedNumber }}</p>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div v-else>
-          <div v-for="hostel in hostels">
-            <h2 class="font-extrabold text-2xl max-sm:text-xl">
-              {{ hostel.name }}
-            </h2>
-            <div
-              v-for="(room, roomIndex) in hostel.rooms"
-              :key="`room_${roomIndex}_bed_${bedIndex}`"
-            >
-              <div
-                v-for="(bed, bedIndex) in room.beds"
-                :key="`room_${roomIndex}_bed_${bedIndex}`"
-              >
-                <div class="text-center text-gray-500">
-                  <div
-                    class="w-10 h-20 max-sm:w-10 max-sm:h-15 mx-auto bg-gray-300 rounded-lg"
-                  ></div>
-                  <h3
-                    class="mb-1 text-2xl max-sm:text-lg font-bold tracking-tight text-gray-900"
-                  >
-                    <a href="#">{{
-                      bed.status === "occupied" ? "Occupied" : "Vacant"
-                    }}</a>
-                  </h3>
-                  <p>Bed - {{ bed.bedNumber }}</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+            <span class="ml-8">{{ index + 1 }}</span>
+          </td>
+          <td
+            :class="[
+              commonTableDataClasses,
+              'uppercase font-medium text-gray-900 cursor-pointer',
+            ]"
+          >
+            <span>{{ slot.saharaHostelNumber }}</span>
+          </td>
+          <td :class="commonTableDataClasses">
+            {{ slot.roomNumber }}
+          </td>
+          <td :class="commonTableDataClasses">
+            {{ slot.bedNumber }}
+          </td>
+          <td :class="commonTableDataClasses">
+            {{ slot.sharingRoom }}
+          </td>
+          <td :class="[commonTableDataClasses, 'bg-red-600 text-white']">
+            Under Construction
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </section>
 </template>
 
 <script setup>
 import { commonClasses } from "../../Common/commonClass";
 import { MagnifyingGlassIcon } from "@heroicons/vue/20/solid";
-import { TrashIcon } from "@heroicons/vue/24/outline";
-import { ref } from "vue";
+import { ref, onBeforeMount, computed } from "vue";
 import { RouteNames } from "@/router";
-import { hostels } from "@/components/modules/Slots/temporaryData.js";
 import { useStore } from "@/store";
+import axios from "axios";
 
 const store = useStore();
 const search = ref("");
+const slots = ref([]);
+
+onBeforeMount(async () => {
+  try {
+    store.isLoading = true;
+    const response = await axios.get(
+      "https://sahara-api-f8yp.vercel.app/allInmates"
+    );
+    console.log(response.data);
+    slots.value = response.data;
+  } catch (error) {
+    console.error("Error fetching inmates:", error);
+  } finally {
+    store.isLoading = false;
+  }
+});
+
+console.log(store.selectedHostel);
+
+const commonHeaderClasses = computed(() => {
+  return "p-3 text-center text-sm font-semibold";
+});
+
+const commonAnchorClasses = computed(() => {
+  return "group inline-flex font-bold text-white";
+});
+
+const commonTableDataClasses = computed(() => {
+  return " whitespace-nowrap py-4 px-3 text-sm text-gray-600";
+});
 </script>
